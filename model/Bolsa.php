e96fe5d8d73538e1ea5cf54b1ececa0098bd17b8
Rafael Souza <rafa.nds13@gmail.com>
2024-01-30 15:20:32 -0300

    adicionando a model 'Bolsa'

---
+++ model/Bolsa.php
@@ -0,0 +1,16 @@
+<?php
+
+class Bolsa{
+    private $idBolsa;
+    private $tipo;
+
+    public function getIdBolsa(){
+        return $this->idBolsa;
+    }
+
+    public function getTipoBolsa(){
+        return $this->tipo;
+    }
+}
+
+?>
