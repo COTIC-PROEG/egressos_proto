02453f8e4d4258a73f386a542cdf3ba67cd4f951
Rafael Souza <rafa.nds13@gmail.com>
2024-01-30 14:51:42 -0300

    adicionando a model 'Genero'

---
+++ model/Genero.php
@@ -0,0 +1,16 @@
+<?php
+
+class Genero{
+    private $idGenero;
+    private $tipo;
+
+    public function getIdGenero(){
+        return $this->idGenero;
+    }
+
+    public function getTipo(){
+        return $this->tipo;
+    }
+}
+
+?>
