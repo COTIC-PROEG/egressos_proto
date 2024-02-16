82fcd2fbedf2b3e1e5cb189af1f0b63c17dc947e
Rafael Souza <rafa.nds13@gmail.com>
2024-02-16 17:04:14 -0300

    Refinando

---
+++ controller/PessoaController.php
@@ -0,0 +1,13 @@
+<?php
+include_once '../model/class/Pessoa.php'
+class PessoaController(){
+    public function acessarFormulario(){
+
+    }
+
+    private function cadastrarPessoa(){
+        
+    }
+}
+
+?>
