38831c63eb269afb68cbf4fb09d9b0e616f9db08
Rafael Souza <rafa.nds13@gmail.com>
2024-04-10 17:02:20 -0300

    Finalizando as operações da classe Cota

--- model/database/CotaDao.php
+++ model/database/CotaDao.php
@@ -3,16 +3,27 @@
 class CotaDao extends Dao{
 
     public function getCotaByName($tipoCota){
-
+        $sql = "SELECT idCota FROM cota WHERE tipoCota = ?";
+        $this->setParams($tipoCota);
+        $this->execute($sql);
+        $result = $this->stmt->get_result();
+        return $this->getId($result);
     }
-
-    public function insertEgressoCota($idEgresso, $idFormaEgresso){
-
-    }
-
     public function insertCota($tipoCota){
-        
+        $sql = "INSERT INTO cota(tipoCota) VALUES(?)";
+        $this->setParams($tipoCota);
+        $this->execute($sql);
+        return $this->getStmtId();
     }
+
+    public function insertEgressoCota($idEgresso, $idCota){
+        $sql = "INSERT INTO egresso_cota(idEgresso, idCota) VALUES(?, ?)";
+        $this->setParams($idEgresso);
+        $this->setParams($idCota);
+        $this->execute($sql);
+        return $this->getStmtId();
+    }
+
 }
 
 ?>
